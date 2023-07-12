c71a7263879e145f2721ec4df4885e0ccbb12c30
dmaax <dudzcool@gmail.com>
2023-07-12 18:47:26 -0300

    feat: Update ItemCreator method

--- src/main/java/com/poompk/lobbypresentsrenewed/utils/ItemCreator.java
+++ src/main/java/com/poompk/lobbypresentsrenewed/utils/ItemCreator.java
@@ -1,35 +1,40 @@
 package com.poompk.lobbypresentsrenewed.utils;
 
 import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import com.poompk.lobbypresentsrenewed.LobbyPresentsRenewed;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
+import java.lang.reflect.Field;
 import java.util.UUID;
 
 public class ItemCreator {
 
-    private final LobbyPresentsRenewed instance;
-    private static String SKULL_MATERIAL;
-
-    public ItemCreator(LobbyPresentsRenewed instance) {
-        this.instance = instance;
-        if (instance.SERVER_VERSION < 13) {
-            SKULL_MATERIAL = "SKULL_ITEM";
-        } else {
-            SKULL_MATERIAL= "LEGACY_SKULL_ITEM";
-        }
+    private static String skullMaterial() {
+        LobbyPresentsRenewed plugin = LobbyPresentsRenewed.getInstance();
+        return (plugin.SERVER_VERSION < 13) ? "SKULL_ITEM" : "LEGACY_SKULL_ITEM";
     }
 
     public static ItemStack createSkull(String url, String name) {
-        ItemStack skullStack = new ItemStack(Material.matchMaterial(SKULL_MATERIAL), 1, (short) 3);
+        ItemStack skullStack = new ItemStack(Material.matchMaterial(skullMaterial()), 1, (short) 3);
         if (url.isEmpty()) return skullStack;
 
         SkullMeta skullMeta = (SkullMeta) skullStack.getItemMeta();
         GameProfile gameProfile = new GameProfile(UUID.randomUUID(), null);
-        ItemMeta itemMeta = skullStack.getItemMeta();
+        gameProfile.getProperties().put("textures", new Property("textures", url));
+
+        try {
+            Field profileField = skullMeta.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(skullMeta, gameProfile);
+        }
+        catch (IllegalArgumentException | NoSuchFieldException | SecurityException | IllegalAccessException exception) {
+            exception.printStackTrace();
+        }
+
+        skullStack.setItemMeta(skullMeta);
         return skullStack;
     }
 
